5041dbba0bcfc12dd078da81da16f3b94e178e22
Diba Mirza <dimirza@cs.ucsb.edu>
2018-03-01 16:52:15 -0800

    Create words.h

---
+++ words.h
@@ -0,0 +1,17 @@
+class Words
+    {
+    public:
+        Words();
+        
+        void append(std::string word);
+        std::string& operator[] (unsigned int index);
+        
+        unsigned int size() const;
+        unsigned int get_capacity() const;
+        std::string operator[] (unsigned int index) const;
+        
+    private:
+        std::string data[10]; // max words is 10 in ver. 1
+        unsigned int used;
+        unsigned int capacity;
+    };
